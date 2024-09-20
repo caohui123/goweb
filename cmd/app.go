d0c9f2066b0c0b14dfd97b5443d2aad430f70a53
tmaa0019 <tmaa0019@student.monash.edu>
2024-09-20 15:00:18 +0800

    head version

--- cmd/app.go
+++ cmd/app.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	gin "github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/caohui123/goweb/router"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World!",
-		})
-	})
-	router.Run(":8080")
+	apiRouter := router.NewApiRouter()
+	apiRouter.Run(":8080")
 }
--- internal/router/api.go
+++ internal/router/api.go
@@ -1,1 +1,25 @@
 package router
+
+import "github.com/gin-gonic/gin"
+
+type APIRouter struct {
+	Router *gin.Engine
+}
+
+func NewAPIRouter() *APIRouter {
+	router := gin.Default()
+	apiRouter := APIRouter{Router: router}
+	apiRouter.initRoutes()
+
+	return &apiRouter
+}
+
+func (api *APIRouter) initRoutes() {
+	api.Router.GET("/ping", api.ping)
+}
+
+func (api *APIRouter) ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
